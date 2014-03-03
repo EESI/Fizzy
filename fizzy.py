7b01272e8fbfab524b88f4fd50512e8c2a735db5
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-03-03 11:28:05 -0500

    clean up the parser descriptions and misc formatting

--- fizzy.py
+++ fizzy.py
@@ -94,13 +94,13 @@ def load_map(fname):
   return meta_data
 
 def get_fs_methods():
-    """
-        get_fs_methods()
-        return the feature selection methods that are 
-        available for use in a list. note that the options
-        are case sensitive. 
-    """
-    return ['CIFE','CMIM','CondMI','Condred','ICAP','JMI','MIM','MIFS','mRMR']
+  """
+  get_fs_methods()
+  return the feature selection methods that are 
+  available for use in a list. note that the options
+  are case sensitive. 
+  """
+  return ['CIFE','CMIM','CondMI','Condred','ICAP','JMI','MIM','MIFS','mRMR']
 
 def convert_to_discrete(items):
   map_dic = {}
@@ -118,27 +118,29 @@ def convert_to_discrete(items):
 
 def run_pyfeast(data, labels, features, args):
   """
-      run_pyfeast(data, labels, method)
-      @data - numpy data (dense)
-      @labels - vector of class labels (discrete)
-      @features - list of feature names
-      @method - feature selection method
-      @n_select - number of features to select
+  run_pyfeast(data, labels, method)
+  @data - numpy data (dense)
+  @labels - vector of class labels (discrete)
+  @features - list of feature names
+  @method - feature selection method
+  @n_select - number of features to select
 
-      The feature selection method is based off of the FEAST 
-      C variable selection toolbox. 
+  The feature selection method is based off of the FEAST 
+  C variable selection toolbox. 
 
-      Reference:
-      Gavin Brown, Adam Pocock, Ming-Jie Zhao, and Mikel Lujan, 
-          "Conditional Likelihood Maximisation: A Unifying Framework 
-          for Information Theoretic Feature Selection," Journal of 
-          Machine Learning Research, vol. 13, pp. 27--66, 2012.
-          (http://jmlr.csail.mit.edu/papers/v13/brown12a.html)
+  Reference:
+  Gavin Brown, Adam Pocock, Ming-Jie Zhao, and Mikel Lujan, "Conditional 
+    Likelihood Maximisation: A Unifying Framework for Information Theoretic 
+    Feature Selection," Journal of Machine Learning Research, vol. 13, 
+    pp. 27--66, 2012.
+    (http://jmlr.csail.mit.edu/papers/v13/brown12a.html)
   """
   try:
     fs_method = getattr(feast, args.fs_method)
   except AttributeError:
-    raise AttributeError("Unknown feature selection method is being specified for PyFeast. Make sure the feature selection method being selected is a valid one. ")
+    raise AttributeError("Unknown feature selection method is being specified "
+        +"for PyFeast. Make sure the feature selection method being selected "
+        +"is a valid one. ")
 
   if len(data.transpose()) < args.select:
     raise ValueError("n_select must be less than the number of observations.")
@@ -156,15 +158,19 @@ def run_pyfeast(data, labels, features, args):
 
 def main():
 
-  #TODO greg fill in descriptions
-  parser = argparse.ArgumentParser(description=
-      "Fizzy implements feature selection for metagenomics \n")
-  parser.add_argument("-n", "--select", type=int, help="number of features to select", default=15)
-  parser.add_argument("-l", "--label", help="name of coulm indicating labels in map file")
-  parser.add_argument("-f", "--fs-method", help="Feature selection method. Available: CIFE CMIM CondMI Condred ICAP JMI MIM MIFS mRMR", default="MIM")
+  parser = argparse.ArgumentParser(description="Fizzy implements feature subset " 
+    +"selection for biological data formats, which are commonly used in metagenomic "
+    +"data analysis.  \n")
+  parser.add_argument("-n", "--select", type=int, help="number of features to select", 
+    default=15)
+  parser.add_argument("-l", "--label", help="name of column of the mapping file that "
+    +"indicates the labels")
+  parser.add_argument("-f", "--fs-method", help="Feature selection method. Available: "
+    +"CIFE CMIM CondMI Condred ICAP JMI MIM MIFS mRMR", default="MIM")
   parser.add_argument("-i", "--input-file", help="biom format file", required=True)
   parser.add_argument("-m", "--map-file", help="CSV mapping file", required=True)
-  parser.add_argument("-o", "--output-file", help="output file where selected OTU IDs are stored", required=True)
+  parser.add_argument("-o", "--output-file", help="output file where selected OTU IDs "
+    +"are stored", required=True)
 
   args = parser.parse_args()
 
